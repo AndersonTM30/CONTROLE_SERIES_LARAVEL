7a638e9446928d2c10d9a0f316197c8c80e37ebf
AndersonTM30 <andersondev30@gmail.com>
2022-11-02 11:14:58 -0300

    Criando a rota de séries sem boas práticas apenas como exemplo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/ola', function() {
-    echo 'Olá Mundo';
-});
+// rota para listar as séries
+Route::get('/series', [SeriesController::class, 'listarSeries']);
